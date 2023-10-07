ddf7ee7f117e9b916e7e01d0992736a0acd21c4b
boitojunior1993 <pomaremcasa93@gmail.com>
2023-10-07 18:13:37 +0200

    Criação de um projeto.

---
+++ meu-primeiro-java-projeto/src/programando/JavinhaJunior.java
@@ -0,0 +1,11 @@
+package programando;
+
+public class JavinhaJunior {
+	public static void main(String[]args) {
+		
+		System.out.println("emerson boito abencoado por DEUS");
+		System.out.println("ola ola ola ola ola ");
+	}
+	
+
+}
